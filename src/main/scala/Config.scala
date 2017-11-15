91d72efe667f94fe5b11ababdad1292a5db95280
Ronny-K <veronikaselenelune@gmail.com>
2017-11-15 23:33:41 +0200

    124+

--- src/main/scala/Config.scala
+++
@@ -1,9 +0,0 @@
-/**
-  * Created by inoquea on 14.11.17.
-  */
-package model
-object Config{
-  val url = "jdbc:postgresql://localhost/airport?user=inoquea&password=11111111"
-  val jdbcDriver =  "org.postgresql.Driver"
-  val slickProfile = MyPostgresDriver
-}
